f56a2f929e938f284e4a2bc1022a1e28493dc662
Hans Habraken <hanshabraken@macbook-pro-2.iconos.be>
2018-11-05 10:40:50 +0100

    bug fixes

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@
 # - Vars file
 
 # Get GUID and export as GUID on all hosts
-ansible localhost,all -m shell -a 'export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc'
+export GUID=`hostname | cut -d"." -f2`
 
 
 # Replace $GUID variable in hosts file with correct GUID
 sed -i "s/\$GUID/${GUID}/g" ansible/hosts
 
 # Execute checks
-ansible all -m ping
-ansible hosts -a "systemctl status docker"
-ansible nodes -m yum -a 'list=atomic-openshift-node'
+#ansible all -m ping
+#ansible hosts -a "systemctl status docker"
+#ansible nodes -m yum -a 'list=atomic-openshift-node'
 
 
 # Execute prerequisites
-ansible-playbook -f 20 -i /ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -f 20 -i ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 
 # Install openshift openshift cluster
-ansible-playbook -f 20 -i /ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -f 20 -i ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
 
 # After install, copy htpasswd file to master2 and master3
