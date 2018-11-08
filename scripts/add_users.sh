90a8358e67065a2a884e6ec048213c6243499ef4
Hans Habraken <hans@earth.iconos.be>
2018-11-08 13:15:53 +0100

    update

--- scripts/add_users.sh
+++ scripts/add_users.sh
@@ -1,5 +1,5 @@
 
-ansible master -b -a "htpasswd -b /etc/origin/master/htpasswd amy 1234"
-ansible master -b -a "htpasswd -b /etc/origin/master/htpasswd andrew 1234"
-ansible master -b -a "htpasswd -b /etc/origin/master/htpasswd brian 1234"
-ansible master -b -a "htpasswd -b /etc/origin/master/htpasswd betty 1234"
+ansible masters -b -a "htpasswd -b /etc/origin/master/htpasswd amy 1234"
+ansible masters -b -a "htpasswd -b /etc/origin/master/htpasswd andrew 1234"
+ansible masters -b -a "htpasswd -b /etc/origin/master/htpasswd brian 1234"
+ansible masters -b -a "htpasswd -b /etc/origin/master/htpasswd betty 1234"
